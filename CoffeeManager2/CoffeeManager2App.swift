8b0c394b6b010290e7bd39e6a0d3fffe3af741bd
Aziz Arfaoui <aziz99arfaoui@gmail.com>
2025-11-10 23:09:52 +0100

    Initial Commit

---
+++ CoffeeManager2/CoffeeManager2App.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeManager2App.swift
+//  CoffeeManager2
+//
+//  Created by Aziz Arfaoui on 10/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeManager2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
